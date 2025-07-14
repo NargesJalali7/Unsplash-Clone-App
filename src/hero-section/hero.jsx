de261644557cb1d1f057a5be543a24083eab0475
Narges Jalali <narges.jalali726@gmail.com>
2025-07-14 17:04:31 +0330

    Hero Section Background Picture

--- src/hero-section/hero.jsx
+++ src/hero-section/hero.jsx
@@ -2,51 +2,55 @@ import { useEffect, useState } from "react";
 import "./hero.css";
 
 export default function HeroSection() {
-  const [heroBackground, handleHeroBackground] = useState([]);
+  const [heroBackground, setHeroBackground] = useState([]);
+  const [currentHeroBackground, setCurrentHeroBackground] = useState(0);
 
   useEffect(() => {
     const accessKey = "e-BkdQ9oYva6jfie_WTzp-U2AP_H7ltt1ZLKDybO6d0";
 
     fetch(`https://api.unsplash.com/photos?per_page=30&client_id=${accessKey}`)
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((pictureData) => {
-        console.log("Fetched photos:", pictureData);
-        handleHeroBackground(pictureData);
+        setHeroBackground(pictureData);
       })
       .catch((error) => {
         console.error("There was a problem with the fetch operation:", error);
       });
   }, []);
 
+  useEffect(() => {
+    if (heroBackground.length === 0) return;
+
+    const intervalId = setInterval(() => {
+      setCurrentHeroBackground(
+        (prevIndex) => (prevIndex + 1) % heroBackground.length
+      );
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [heroBackground]);
+
+  const width = 1600;
+  const height = 900;
+
   return (
-    <>
-      {heroBackground.length > 0 && (
+    <div className="hero-background">
+      {heroBackground.map((img, index) => (
         <div
+          key={img.id}
+          className={`hero-image ${
+            index === currentHeroBackground ? "visible" : ""
+          }`}
           style={{
-            width: "100%",
-            height: "60vh",
-            backgroundImage: `url(${heroBackground[0].urls.full})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            color: "white",
-            fontSize: "3rem",
-            fontWeight: "bold",
-            textShadow: "0 0 10px rgba(0,0,0,0.6)",
-            flexDirection: "column",
+            backgroundImage: `url(${img.urls.raw}?w=${width}&h=${height}&fit=crop&crop=entropy)`,
           }}
         >
-          <h1 style={{ marginBottom: "0.1rem" }}>Unsplash</h1>
-          <p style={{ marginBottom: "0.7rem" }}>
-            The most powerful photo engine in the world.
-          </p>
+          <div className="hero-content">
+            <h1>Unsplash</h1>
+            <p>The most powerful photo engine in the world.</p>
+          </div>
         </div>
-      )}
-    </>
+      ))}
+    </div>
   );
 }
